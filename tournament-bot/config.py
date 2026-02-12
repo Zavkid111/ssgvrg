1c04b61799401587b827c287d2713b59a97f9aa3
Zavkid111 <zavkiddinabdulloev13@gmail.com>
2026-02-12 15:01:23 +0700

    Refactor config.py to streamline variable loading

--- tournament-bot/config.py
+++ tournament-bot/config.py
@@ -1,10 +1,8 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
+# Переменные будут автоматически подтягиваться из панели Bothost
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS").split(",")))
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS").split(",")))  # если несколько ID через запятую
 DB_PATH = "database/database.db"
 
 DEFAULT_REQUISITES = """
