a445b3d6ed3a4471c4562894297b5ec0289cb76c
Zavkid111 <zavkiddinabdulloev13@gmail.com>
2026-02-12 18:49:05 +0700

    Refactor config.py to handle missing ADMIN_IDS

--- tournament-bot/config.py
+++ tournament-bot/config.py
@@ -1,8 +1,8 @@
 import os
 
-# Переменные будут автоматически подтягиваться из панели Bothost
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS").split(",")))  # если несколько ID через запятую
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
+
 DB_PATH = "database/database.db"
 
 DEFAULT_REQUISITES = """
